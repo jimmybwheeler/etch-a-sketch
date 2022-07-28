8238dd68dc08c916aa24df51c59b5f7e4dff4647
James Wheeler <jimmybwheeler@gmail.com>
2022-07-28 14:37:47 -0700

    updated the one box

--- script.js
+++ script.js
@@ -3,8 +3,12 @@ const squares = document.createElement("div");
 squares.classList.add("squares");
 
 for(let x = 0; x < 16; x++){
-    main.appendChild(squares);
-    console.log(container);
+    for(let y = 0; y < 16; y++){
+        main.appendChild(squares);
+        squares.style.width = "25px";
+        squares.style.height = "25px";
+        squares.style.background = "orange";
+    }
 }
     
 
