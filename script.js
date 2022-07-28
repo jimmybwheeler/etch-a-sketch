181cde7d25a7d3d9d265a1c1db14a8fa5add5544
James Wheeler <jimmybwheeler@gmail.com>
2022-07-28 14:24:05 -0700

    got one box to show up for grid

---
+++ script.js
@@ -0,0 +1,10 @@
+const main = document.querySelector(".container");
+const squares = document.createElement("div");
+squares.classList.add("squares");
+
+for(let x = 0; x < 16; x++){
+    main.appendChild(squares);
+    console.log(container);
+}
+    
+
