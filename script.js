15fe8fe57129157415886c1536361d8c32a9660c
James Wheeler <jimmybwheeler@gmail.com>
2022-07-29 14:13:32 -0700

    only one square changes color

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 const main = document.querySelector(".container");
-const squares = document.createElement("div");
-squares.classList.add("squares");
+let squares;
 
 for(let x = 0; x < 16; x++){
     for(let y = 0; y < 16; y++){
+        squares = document.createElement("div");
+        squares.classList.add("squaresCl");
         main.appendChild(squares);
-        squares.style.width = "25px";
-        squares.style.height = "25px";
-        squares.style.background = "orange";
+        squares.style.width = "50px";
+        squares.style.height = "50px";
+        squares.style.background = "lightgray";
     }
 }
-    
 
+function getRandomColor() {
+    let letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+//event listeners
+squares.addEventListener("mouseover", (e) => {
+    let newColor = getRandomColor();
+    return e.target.style.backgroundColor = newColor;
+});
