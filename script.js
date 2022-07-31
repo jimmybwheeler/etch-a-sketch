daaf01a7df9f6877e3acc0790280ba70b97842df
James Wheeler <jimmybwheeler@gmail.com>
2022-07-31 13:14:06 -0700

    not completed

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 const main = document.querySelector(".container");
-let squares;
 
 for(let x = 0; x < 16; x++){
     for(let y = 0; y < 16; y++){
         squares = document.createElement("div");
-        squares.classList.add("squaresCl");
+        squares.classList.add("squares");
         main.appendChild(squares);
-        squares.style.width = "50px";
-        squares.style.height = "50px";
-        squares.style.background = "lightgray";
     }
 }
 
