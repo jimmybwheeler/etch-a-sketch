dff52036f86bc587ec24af2b9a71f914cb49248d
James Wheeler <jimmybwheeler@gmail.com>
2022-08-01 14:22:03 -0700

    started grid

---
+++ script.js
@@ -0,0 +1,13 @@
+function createGrid(gridSize){
+    for(let i = 0; i < gridSize; i++){
+        const main = document.querySelector(".container");
+        const grid = document.createElement("div");
+        grid.classList.add("squares");
+        grid.style.height = "50px";
+        grid.style.width = "50px";
+        grid.style.border = "solid black 2px";
+        main.appendChild(grid);
+    }
+}
+
+createGrid(256);
