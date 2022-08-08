e153d62fad362b2b4ca753a72d289f807ab1da04
James Wheeler <jimmybwheeler@gmail.com>
2022-08-08 14:14:43 -0700

    found a solution online to one of my problems

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-function createGrid(gridSize){
-    for(let i = 0; i < gridSize; i++){
-        const main = document.querySelector(".container");
-        let grid = document.createElement("div");
-        main.appendChild(grid).className = "squares";
+const container = document.querySelector(".container");
+let rows = document.getElementsByClassName("gridRow");
+let cells = document.getElementsByClassName("cell");
+
+defaultGrid();
+
+function defaultGrid(){
+    makeRows(16);
+    makeColumns(16);
+}
+
+function makeRows(rowNum){
+    for(r = 0; r < rowNum; r++){
+        let row = document.createElement("div");
+        container.appendChild(row).className = "gridRow";
     }
 }
 
-createGrid(256);
+function makeColumns(cellNum){
+    for(i = 0; i < rows.length; i++){
+        for(j = 0; j < cellNum; j++){
+            let newCell = document.createElement("div");
+            rows[j].appendChild(newCell).className = "cell";
+        }
+    }
+}
