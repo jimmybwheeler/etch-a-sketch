062f86928b5eb29e1dcaace434d7cd9737056f24
James Wheeler <jimmybwheeler@gmail.com>
2022-08-07 17:37:10 -0700

    moved things around

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
 function createGrid(gridSize){
     for(let i = 0; i < gridSize; i++){
         const main = document.querySelector(".container");
-        const grid = document.createElement("div");
-        grid.classList.add("squares");
-        grid.style.height = "50px";
-        grid.style.width = "50px";
-        grid.style.border = "solid black .01px";
-        main.appendChild(grid);
+        let grid = document.createElement("div");
+        main.appendChild(grid).className = "squares";
     }
 }
 
